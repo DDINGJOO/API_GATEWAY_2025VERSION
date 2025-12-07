c7fdcaa59549c5ca76d88d1d1a6ca198be06da2e
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-07 13:38:06 +0900

    [FEAT] 가격 정책 DTO 추가

---
+++ src/main/java/com/study/api_gateway/dto/pricing/response/PlacePricingBatchResponse.java
@@ -0,0 +1,29 @@
+package com.study.api_gateway.dto.pricing.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Place 기반 가격 정책 배치 조회 응답
+ * 특정 Place에 속한 모든 Room의 가격 정책 정보
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PlacePricingBatchResponse {
+
+    /**
+     * 장소 ID
+     */
+    private Long placeId;
+
+    /**
+     * Room별 가격 정보 리스트
+     */
+    private List<RoomPricingInfo> rooms;
+}
---
+++ src/main/java/com/study/api_gateway/dto/pricing/response/RoomsPricingBatchResponse.java
@@ -0,0 +1,23 @@
+package com.study.api_gateway.dto.pricing.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Room ID 리스트 기반 가격 정책 배치 조회 응답
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomsPricingBatchResponse {
+
+    /**
+     * Room별 가격 정보 리스트
+     */
+    private List<RoomPricingInfo> rooms;
+}
