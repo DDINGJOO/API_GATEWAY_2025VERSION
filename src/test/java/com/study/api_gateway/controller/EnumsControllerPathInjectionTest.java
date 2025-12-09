e15ad24bc6256d5ba6cb3dc90f85b480ad10a472
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:53:13 +0900

    [REFACTOR] EnumsControllerPathInjectionTest 코드 포맷팅 정리

--- src/test/java/com/study/api_gateway/controller/EnumsControllerPathInjectionTest.java
+++ src/test/java/com/study/api_gateway/controller/EnumsControllerPathInjectionTest.java
@@ -22,35 +22,35 @@ import java.util.Map;
 @WebFluxTest(controllers = EnumsController.class)
 @Import({ResponseFactory.class, RequestPathHelper.class, GlobalExceptionHandler.class, com.study.api_gateway.config.TestConfig.class})
 class EnumsControllerPathInjectionTest {
-
-    @Autowired
-    private WebTestClient webTestClient;
-
-    @MockBean
-    private ProfileClient profileClient;
-
-    @MockBean
-    private AuthClient authClient;
-
-    @MockBean
-    private ImageClient imageClient;
-
-    @Test
-    @DisplayName("X-Forwarded-Prefix + X-Forwarded-Uri 헤더가 결합되어 응답.request.path/url 에 반영된다")
-    void pathInjectedFromForwardedHeaders() {
-        Mockito.when(profileClient.fetchGenres())
-                .thenReturn(Mono.just(Map.of(1, "Rock")));
-
-        webTestClient.get()
-                .uri("/bff/v1/enums/genres")
-                .header("X-Forwarded-Prefix", "/gateway")
-                .header("X-Forwarded-Uri", "/v1/enums/genres?lang=ko")
-                .header("X-Forwarded-Proto", "http")
-                .header("X-Forwarded-Host", "gw.example.com")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.request.path").isEqualTo("/gateway/v1/enums/genres?lang=ko")
-                .jsonPath("$.request.url").isEqualTo("http://gw.example.com/gateway/v1/enums/genres?lang=ko");
-    }
+	
+	@Autowired
+	private WebTestClient webTestClient;
+	
+	@MockBean
+	private ProfileClient profileClient;
+	
+	@MockBean
+	private AuthClient authClient;
+	
+	@MockBean
+	private ImageClient imageClient;
+	
+	@Test
+	@DisplayName("X-Forwarded-Prefix + X-Forwarded-Uri 헤더가 결합되어 응답.request.path/url 에 반영된다")
+	void pathInjectedFromForwardedHeaders() {
+		Mockito.when(profileClient.fetchGenres())
+				.thenReturn(Mono.just(Map.of(1, "Rock")));
+		
+		webTestClient.get()
+				.uri("/bff/v1/enums/genres")
+				.header("X-Forwarded-Prefix", "/gateway")
+				.header("X-Forwarded-Uri", "/v1/enums/genres?lang=ko")
+				.header("X-Forwarded-Proto", "http")
+				.header("X-Forwarded-Host", "gw.example.com")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$.request.path").isEqualTo("/gateway/v1/enums/genres?lang=ko")
+				.jsonPath("$.request.url").isEqualTo("http://gw.example.com/gateway/v1/enums/genres?lang=ko");
+	}
 }
