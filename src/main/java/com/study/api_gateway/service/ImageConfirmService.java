0ac40e9a2720f23975b1352dc29bafad2759484b
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:53:12 +0900

    [REFACTOR] ImageConfirmService 코드 포맷팅 정리

--- src/main/java/com/study/api_gateway/service/ImageConfirmService.java
+++ src/main/java/com/study/api_gateway/service/ImageConfirmService.java
@@ -9,19 +9,19 @@ import java.util.List;
 
 @Service
 public class ImageConfirmService {
-
-    private final ImageClient imageClient;
-
-    public ImageConfirmService(ImageClient imageClient) {
-        this.imageClient = imageClient;
-    }
-    public Mono<Void> confirmImage(String referenceId, List<String> imageIds)
-    {
-	    if (imageIds.size() == 1) {
-		    return imageClient.confirmImage(referenceId, imageIds.get(0));
-	    }
-        return imageClient.confirmImage(referenceId,imageIds);
-    }
-
-
+	
+	private final ImageClient imageClient;
+	
+	public ImageConfirmService(ImageClient imageClient) {
+		this.imageClient = imageClient;
+	}
+	
+	public Mono<Void> confirmImage(String referenceId, List<String> imageIds) {
+		if (imageIds.size() == 1) {
+			return imageClient.confirmImage(referenceId, imageIds.get(0));
+		}
+		return imageClient.confirmImage(referenceId, imageIds);
+	}
+	
+	
 }
