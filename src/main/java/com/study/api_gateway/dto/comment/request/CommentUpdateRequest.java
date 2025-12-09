ede1c379e5eb474d3f53eb4dac2fe81b50d0d43b
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:53:10 +0900

    [REFACTOR] CommentUpdateRequest 코드 포맷팅 정리

--- src/main/java/com/study/api_gateway/dto/comment/request/CommentUpdateRequest.java
+++ src/main/java/com/study/api_gateway/dto/comment/request/CommentUpdateRequest.java
@@ -8,7 +8,7 @@ import lombok.Data;
 		example = "{\n  \"contents\": \"수정한 내용\"\n}")
 public class CommentUpdateRequest {
 	@Schema(description = "작성자 ID (서버에서 토큰으로부터 자동 설정, 클라이언트는 전송 불필요)", example = "user-1", accessMode = Schema.AccessMode.READ_ONLY)
-    private String writerId;
+	private String writerId;
 	@Schema(description = "수정할 댓글 내용", example = "수정한 내용")
-    private String contents;
+	private String contents;
 }
