a9b20765fcb45bb7029af47bcbb2644b1c40d4de
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:53:12 +0900

    [REFACTOR] RoomDetailResponse 코드 포맷팅 정리

--- src/main/java/com/study/api_gateway/dto/room/response/RoomDetailResponse.java
+++ src/main/java/com/study/api_gateway/dto/room/response/RoomDetailResponse.java
@@ -30,22 +30,23 @@ public class RoomDetailResponse {
 	private Integer maxOccupancy;
 	private List<String> furtherDetails;
 	private List<String> cautionDetails;
-
+	
 	/**
 	 * 새로운 이미지 구조 (imageId, imageUrl, sequence 포함)
 	 */
 	private List<ImageInfo> images;
-
+	
 	/**
 	 * 기존 이미지 URL 목록 (하위 호환성을 위해 유지)
+	 *
 	 * @deprecated 향후 제거 예정. images 필드 사용 권장
 	 */
 	@Deprecated
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	private List<String> imageUrls;
-
+	
 	private List<Long> keywordIds;
-
+	
 	/**
 	 * images 필드로부터 imageUrls를 자동 생성 (하위 호환성)
 	 */
