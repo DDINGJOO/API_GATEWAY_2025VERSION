631addc4476c7ac94288b5cd7a4d83653e8acc23
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:53:11 +0900

    [REFACTOR] CouponDownloadRequest 코드 포맷팅 정리

--- src/main/java/com/study/api_gateway/dto/coupon/request/CouponDownloadRequest.java
+++ src/main/java/com/study/api_gateway/dto/coupon/request/CouponDownloadRequest.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class CouponDownloadRequest {
-    private String couponCode;
-    private Long userId;
+	private String couponCode;
+	private Long userId;
 }
