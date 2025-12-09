49963f64df305b670d518a30e70ff7ba0d316405
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:53:09 +0900

    [REFACTOR] AuthClient 코드 포맷팅 정리

--- src/main/java/com/study/api_gateway/client/AuthClient.java
+++ src/main/java/com/study/api_gateway/client/AuthClient.java
@@ -16,106 +16,107 @@ import java.util.Map;
 
 @Component
 public class AuthClient {
-    private final WebClient webClient;
-
-    public AuthClient(@Qualifier(value = "authWebClient") WebClient webClient) {
-        this.webClient = webClient;
-    }
-
-
-    public Mono<LoginResponse> login(String email, String password) {
-	    
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/login")
-                .toUriString();
-
-
-        return webClient.post()
-                .uri(uriString)
-                .bodyValue(new LoginRequest(email, password))
-                .retrieve()
-                .bodyToMono(LoginResponse.class);
-    }
-
-    public Mono<Boolean> signup(String email, String password, String passwordConfirm, List<ConsentRequest> consentReq) {
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/signup")
-                .toUriString();
-
-        return webClient.post()
-                .uri(uriString)
-                .bodyValue(new SignupRequest(email, password, passwordConfirm, consentReq))
-                .retrieve()
-                .bodyToMono(Boolean.class);
-    }
-
-    public Mono<Boolean> confirmEmail(String email,  String code) {
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/emails/{email}")
-                .queryParam("code", code)
-                .buildAndExpand(email)
-                .toUriString();
-
-
-        return webClient.get()
-                .uri(uriString)
-                .retrieve()
-                .bodyToMono(Boolean.class);
-    }
-
-    public Mono<Void> sendCode(String email) {
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/emails/{email}")
-                .buildAndExpand(email)
-                .toUriString();
-
-        return webClient.post()
-                .uri(uriString)
-                .retrieve()
-                .bodyToMono(Void.class);
-    }
-
-    public Mono<LoginResponse> refreshToken(TokenRefreshRequest req) {
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/login/refreshToken")
-                .toUriString();
-
-
-        return webClient.post()
-                .uri(uriString)
-                .bodyValue(req)
-                .retrieve()
-                .bodyToMono(LoginResponse.class);
-    }
-
-    public Mono<Boolean> changePassword(PasswordChangeRequest req) {
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/passwordChange/changePassword")
-                .toUriString();
-
-        return webClient.put()
-                .uri(uriString)
-                .bodyValue(req)
-                .retrieve()
-                .bodyToMono(Boolean.class);
-    }
-
-    public Mono<Boolean> withdraw(String userId, String withdrawReason) {
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/withdraw/{userId}")
-                .queryParam("withdrawReason", withdrawReason)
-                .buildAndExpand(userId)
-                .toUriString();
-
-        return webClient.delete()
-                .uri(uriString)
-                .retrieve()
-                .bodyToMono(Boolean.class);
-    }
-
-    public Mono<Map<String, ConsentsTable>> fetchAllConsents(Boolean all){
-	    String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/consents")
-                .queryParam("all", all)
-                .toUriString();
-
-        return webClient.get()
-                .uri(uriString)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Map<String, ConsentsTable>>() {});
-    }
+	private final WebClient webClient;
+	
+	public AuthClient(@Qualifier(value = "authWebClient") WebClient webClient) {
+		this.webClient = webClient;
+	}
+	
+	
+	public Mono<LoginResponse> login(String email, String password) {
+		
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/login")
+				.toUriString();
+		
+		
+		return webClient.post()
+				.uri(uriString)
+				.bodyValue(new LoginRequest(email, password))
+				.retrieve()
+				.bodyToMono(LoginResponse.class);
+	}
+	
+	public Mono<Boolean> signup(String email, String password, String passwordConfirm, List<ConsentRequest> consentReq) {
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/signup")
+				.toUriString();
+		
+		return webClient.post()
+				.uri(uriString)
+				.bodyValue(new SignupRequest(email, password, passwordConfirm, consentReq))
+				.retrieve()
+				.bodyToMono(Boolean.class);
+	}
+	
+	public Mono<Boolean> confirmEmail(String email, String code) {
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/emails/{email}")
+				.queryParam("code", code)
+				.buildAndExpand(email)
+				.toUriString();
+		
+		
+		return webClient.get()
+				.uri(uriString)
+				.retrieve()
+				.bodyToMono(Boolean.class);
+	}
+	
+	public Mono<Void> sendCode(String email) {
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/emails/{email}")
+				.buildAndExpand(email)
+				.toUriString();
+		
+		return webClient.post()
+				.uri(uriString)
+				.retrieve()
+				.bodyToMono(Void.class);
+	}
+	
+	public Mono<LoginResponse> refreshToken(TokenRefreshRequest req) {
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/login/refreshToken")
+				.toUriString();
+		
+		
+		return webClient.post()
+				.uri(uriString)
+				.bodyValue(req)
+				.retrieve()
+				.bodyToMono(LoginResponse.class);
+	}
+	
+	public Mono<Boolean> changePassword(PasswordChangeRequest req) {
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/passwordChange/changePassword")
+				.toUriString();
+		
+		return webClient.put()
+				.uri(uriString)
+				.bodyValue(req)
+				.retrieve()
+				.bodyToMono(Boolean.class);
+	}
+	
+	public Mono<Boolean> withdraw(String userId, String withdrawReason) {
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/withdraw/{userId}")
+				.queryParam("withdrawReason", withdrawReason)
+				.buildAndExpand(userId)
+				.toUriString();
+		
+		return webClient.delete()
+				.uri(uriString)
+				.retrieve()
+				.bodyToMono(Boolean.class);
+	}
+	
+	public Mono<Map<String, ConsentsTable>> fetchAllConsents(Boolean all) {
+		String uriString = UriComponentsBuilder.fromPath("/api/v1/auth/consents")
+				.queryParam("all", all)
+				.toUriString();
+		
+		return webClient.get()
+				.uri(uriString)
+				.retrieve()
+				.bodyToMono(new ParameterizedTypeReference<Map<String, ConsentsTable>>() {
+				});
+	}
 	
 	public Mono<Boolean> hasPhoneNumber(String userId) {
 		String uriString = UriComponentsBuilder.fromPath("/api/v1/phone-number/{userId}")
