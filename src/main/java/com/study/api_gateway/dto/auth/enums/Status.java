dad3b67ffddadfcb651c4ec777164ad5e3f3c1c3
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:53:10 +0900

    [REFACTOR] Status 코드 포맷팅 정리

--- src/main/java/com/study/api_gateway/dto/auth/enums/Status.java
+++ src/main/java/com/study/api_gateway/dto/auth/enums/Status.java
@@ -1,5 +1,5 @@
 package com.study.api_gateway.dto.auth.enums;
 
 public enum Status {
-    ACTIVE, BLOCKED, DELETED, EXPIRED, UNCONFIRMED, SLEEPING, SUSPENDED
+	ACTIVE, BLOCKED, DELETED, EXPIRED, UNCONFIRMED, SLEEPING, SUSPENDED
 }
