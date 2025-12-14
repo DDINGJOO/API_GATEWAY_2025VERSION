0921fa33b6609379a323006a5b5893a6ad9858c7
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-14 13:55:35 +0900

    [FEAT] ReservationStatus enum 확장

--- src/main/java/com/study/api_gateway/dto/reservationManage/enums/ReservationStatus.java
+++ src/main/java/com/study/api_gateway/dto/reservationManage/enums/ReservationStatus.java
@@ -2,14 +2,12 @@ package com.study.api_gateway.dto.reservationManage.enums;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "예약 상태")
 public enum ReservationStatus {
-	@Schema(description = "결제 대기 중")
-	PENDING_PAYMENT,
-	
-	@Schema(description = "예약 확정")
-	CONFIRMED,
-	
-	@Schema(description = "예약 취소")
-	CANCELLED
+	AWAITING_USER_INFO,  // 추가
+	PENDING,             // 추가
+	PENDING_PAYMENT,     // 기존
+	CONFIRMED,           // 기존
+	REJECTED,            // 추가
+	REFUNDED,            // 추가
+	CANCELLED            // 기존
 }
